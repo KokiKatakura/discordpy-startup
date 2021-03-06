4614599538ae4e8367a951a054e574df7436a843
KokiKatakura <79741249+KokiKatakura@users.noreply.github.com>
2021-03-06 10:50:34 +0900

    202103061050

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,15 @@ from discord.ext import commands
 import os
 import traceback
 
+# インストールした discord.py を読み込む
+import discord
+import random
+import string
+import datetime
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
