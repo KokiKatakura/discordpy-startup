1cdde83e761b81c1af117f6e2b01b3e504e744c9
KokiKatakura <79741249+KokiKatakura@users.noreply.github.com>
2021-03-06 11:08:37 +0900

    202103061108

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,7 @@ import os
 import traceback
 
 # インストールした discord.py を読み込む
-import discord
+#import discord
 import random
 import string
 import datetime
@@ -33,40 +33,14 @@ def GetRandomStr(num = 4):
      # 英数字からランダムに取得
      return ''.join([random.choice(dat) for i in range(num)])
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+@bot.command()
+async def project(ctx):
+    await ctx.send('あなたのプロジェクトのプロジェクトIDは%s%sです。' % (y,z))
+    await ctx.send('下記URLにアクセスしてOKRのフォームを埋めてください。')
+    await ctx.send('https://forms.gle/D1RHXmdNaZwbXhsP7')
+    
 
-# メッセージ受信時に動作する処理
-@client.event.event
-async def on_message(message, number = 1):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-    # 「project」と発言したらprojectのナンバーを発行する。
-    if message.content == 'project' or message.content == 'Project' or message.content == 'PROJECT':
-        # 日付取得
-        y = 0
-        today = datetime.date.today()
-        y = today.strftime("%Y%m%d")
-        # ランダムな英数字取得
-        z = 0
-        num = 4
-        z = GetRandomStr(num)
-        await message.channel.send('あなたのプロジェクトのプロジェクトIDは%s%sです。' % (y,z))
-        await message.channel.send('下記URLにアクセスしてOKRのフォームを埋めてください。')
-        await message.channel.send('https://forms.gle/D1RHXmdNaZwbXhsP7')
-        print('あなたのプロジェクトのプロジェクトIDは%s%sです。' % (y,z))
-        print('下記URLにアクセスしてOKRのフォームを埋めてください。')
-        print('https://forms.gle/D1RHXmdNaZwbXhsP7')
-        # print('%d' % number)
-        # number = number + 1
-        # return number
+
 
 
 bot.run(token)
